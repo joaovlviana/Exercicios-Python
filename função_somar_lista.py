00cc8467c039c7885a99855977878229a6c2c8ff
João Victor Viana <78990729+joaovlviana@users.noreply.github.com>
2021-11-22 14:28:26 -0300

    Delete função_somar_lista.py

--- função_somar_lista.py
+++
@@ -1,8 +0,0 @@
-#Função que retorna a soma de uma lista
-def somar_lista(numeros):
-    soma = 0
-    for i in numeros:
-        soma = soma + i
-    return soma
-
-print(somar_lista([1,3,5,7,9,11]))
