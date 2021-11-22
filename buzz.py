cd1e45bf3b9ba1109652250a2db288a935536032
João Victor Viana <78990729+joaovlviana@users.noreply.github.com>
2021-11-22 14:05:35 -0300

    Update file

--- buzz.py
+++ buzz.py
@@ -1,3 +1,4 @@
+#Pede que o usuário insira um número e, se o mesmo for divisível por 5, imprime "Buzz", caso contrário imprime o próprio número.
 num = int(input("Insira um número inteiro:"))
 
 if num % 5 == 0 :
