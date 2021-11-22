073610966e543c464b28355ea2fb8c1e7113b964
João Victor Viana <78990729+joaovlviana@users.noreply.github.com>
2021-11-22 14:18:10 -0300

    Update and rename exercício 1.py to perimetro_area.py

---
+++ perimetro_area.py
@@ -0,0 +1,8 @@
+#Recebe o lado de um quadrado inserido pelo usuário e retorna área e perímetro
+
+lado = float(input("Insira o lado do quadrado:"))
+perimetro = lado*4
+area = lado**2
+
+print(f"perímetro {perimetro}, área {area}")
+
