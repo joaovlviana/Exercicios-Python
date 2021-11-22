c002cc89af14802e1de8a3b4b300278bcec1c7f8
João Victor Viana <78990729+joaovlviana@users.noreply.github.com>
2021-11-22 14:41:05 -0300

    Delete primeiro jogo.py

--- primeiro jogo.py
+++
@@ -1,9 +0,0 @@
-num = 5
-teste = int(input("Insira um número:"))
-
-if teste > num:
-    input("O valor entrado está acima.")
-elif teste == num:
-    input("O valor entrado está correto!")
-else:
-    input("O valor entrado está abaixo.")
